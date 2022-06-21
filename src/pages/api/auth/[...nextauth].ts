01a2b713d629aa79ca5d75a9b79fcf815dc3e6bf
Joao <paladino160@hotmail.com>
2022-06-21 17:20:45 -0300

    atualização

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -8,9 +11,45 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: "read:user",
+          scope: "read:user, user:email",
         },
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+        const Email = user.email
+       try{
+        await fauna.query(
+         q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            { data: { Email } }
+
+        ),
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(user.email)
+          )
+        )
+         )
+      )
+
+         return true
+       }catch {
+        return false
+       }
+    },
+},
+
 });
